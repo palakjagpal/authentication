bab83785c430f87d1355ca87cd75dbd8ec338a3c
Palak Jagpal <palakjagpal70@gmail.com>
2026-03-20 18:18:59 +0530

    ...

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -3,7 +3,6 @@ import axios from "axios";
 import { useNavigate } from "react-router";
 import "./UserAuth.css";
 
-
 function Profile(){
     const [data, setdata] = useState(null);
     const navigate = useNavigate();
